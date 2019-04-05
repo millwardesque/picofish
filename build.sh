d28e6655acb9580165123565de7671d66b3695bd
Christopher Millward <christopher.millward@gmail.com>
2019-04-05 14:59:33 -0400

    Separated core engine and made build script work with multiple directories.

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-PICOTOOL_DIR=../../picotool
+
+CODE_ROOT=`pwd`
+PICOTOOL_DIR=$CODE_ROOT/../../picotool
 
 > debug.log.p8l
-$PICOTOOL_DIR/p8tool build fish.p8 --lua game.lua --lua-path="?;?.lua;lib/?.lua"
+$PICOTOOL_DIR/p8tool build fish.p8 --lua game.lua --lua-path="$CODE_ROOT/?;$CODE_ROOT/?.lua;$CODE_ROOT/lib/?.lua;$CODE_ROOT/src/?.lua"
